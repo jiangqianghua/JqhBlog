81a0902de30a7ff2d19e96a8958ed7fddc449ecf
jiangqianghua <240437339@qq.com>
2020-07-25 00:28:19 +0800

    完成列表切换

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,33 @@
 // 公共头部组件
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../static/style/components/header.css';
+import servicePath from '../config/apiUrl'
+import axios from 'axios';
+import Router from 'next/router';
 
 import {Row, Col, Menu, Icon} from 'antd'
 const Header = () => {
+    const[navArray, setNavArray] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(servicePath.getTypeInfo).then(
+                (res) => {
+                    // setNavArray(res.data.data)
+                    return res.data.data
+                }
+            )
+            setNavArray(result)
+        }
+        fetchData()
+    }, [])
+
+    const handleClick = (e) => {
+        if (e.key == 0) {
+            Router.push('/index')
+        } else {
+            Router.push('/list?id='+e.key)
+        }
+    }
     return (
         <div className="header">
             <Row type="flex" justify="center">
@@ -12,19 +36,22 @@ const Header = () => {
                     <span className="header-txt">欢迎使用搜索资源网</span>
                 </Col>
                 <Col className="memu-div" xs={0} sm={0} md={14} lg={8} xl={8}>
-                    <Menu  mode="horizontal">
-                        <Menu.Item key="home">
+                    <Menu  mode="horizontal"
+                    onClick={handleClick}>
+                        <Menu.Item key="0">
                             <Icon type="home"/>
                             首页
                         </Menu.Item>
-                        <Menu.Item key="video">
-                            <Icon type="youtube"/>
-                            视频
-                        </Menu.Item>
-                        <Menu.Item key="life">
-                            <Icon type="smile"/>
-                            生活
-                        </Menu.Item>
+                        {
+                            navArray.map((item) => {
+                                return (
+                                    <Menu.Item key={item.id}>
+                                        <Icon type="home"/>
+                                        {item.typeName}
+                                    </Menu.Item>
+                                )
+                            })
+                        }
                     </Menu>
                 </Col>
             </Row>
